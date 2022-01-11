9c87b5651ee7a4d51ac2e08b4e251bcf0441f7b3
Endogenisis <96134799+Endogenisis@users.noreply.github.com>
2022-01-11 14:08:17 +0300

    Созданы первые файлы PajeObject

--- test_main_page.py
+++ test_main_page.py
@@ -2,8 +2,9 @@ from selenium.webdriver.common.by import By
 import time
 link = " http://selenium1py.pythonanywhere.com"
 
-def test_guest_should_see_basket_form(browser):
+def test_guest_can_go_to_login_page(browser):
     browser.get(link)
-    time.sleep(3)
-    button = browser.find_element(By.XPATH, "//*[@id='login_link']")
-    assert button, "Кнопка не найдена"
+    go_to_login_page(browser)
+def go_to_login_page(browser):
+    login_link = browser.find_element(By.XPATH, "//*[@id='login_link']")
+    login_link.click()
