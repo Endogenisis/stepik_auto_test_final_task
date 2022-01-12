93b5a376ee4e200a853308ee6c363eab09346055
Endogenisis <96134799+Endogenisis@users.noreply.github.com>
2022-01-12 11:06:20 +0300

    Вынесли селектор в отдельный файл.

---
+++ pages/locators.py
@@ -0,0 +1,5 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.XPATH, '//*[@id="login_link"]')
--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,6 @@
 import time
 from .pages.main_page import MainPage
-link = " http://selenium1py.pythonanywhere.com"
+link = "http://selenium1py.pythonanywhere.com/"
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
