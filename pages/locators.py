39619adbb8c140fbcb0cde0dab2cfb9657db73a7
Endogenisis <96134799+Endogenisis@users.noreply.github.com>
2022-01-13 19:20:30 +0300

    Создан файл login_page и организованы тесты страницы логина.

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,7 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.XPATH, '//*[@id="login_link"]')
+
+class LoginPageLocators():
+    Login_form = (By.ID, "login_form")
+    Register_form = (By.ID, "register_form")
--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,5 @@
 from .base_page import BasePage
+from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
@@ -8,13 +9,13 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
+        print("Проверка страницы логина.")
+        assert "login" in self.browser.current_url,  "Это не страница логина."
         # реализуйте проверку на корректный url адрес
-        assert True
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        assert True
+        # Проверка наличия формы логина
+        assert self.is_element_present(*LoginPageLocators.Login_form), "Нет формы логина."
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        assert self.is_element_present(*LoginPageLocators.Register_form), "Нет формы регистрации."
