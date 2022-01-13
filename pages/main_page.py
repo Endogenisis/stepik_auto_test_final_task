88926e9f064c5b98e6caf84bd6662382f593fd91
Endogenisis <96134799+Endogenisis@users.noreply.github.com>
2022-01-13 19:50:19 +0300

    Перенёс тесты в оди файл. Добавил переход между страницами.

--- pages/main_page.py
+++ pages/main_page.py
@@ -2,11 +2,13 @@ from .base_page import BasePage
 from .locators import MainPageLocators
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from .login_page import LoginPage
 # Файл поиска элементов на главной странице.
 class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
+        # return LoginPage(browser=self.browser, url=self.browser.current_url) Способ перехода между страницами.
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Ссылка для входа не появилась."
 
--- test_login_page.py
+++ test_login_page.py
@@ -1,9 +1,9 @@
-from .pages.login_page import LoginPage
+#from .pages.login_page import LoginPage
 # Тесты страницы логина.
-link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+#link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
 
-def test_should_be_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_page()
+#def test_should_be_login_page(browser):
+ #   link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+#    page = LoginPage(browser, link)
+#    page.open()
+#    page.should_be_login_page()
